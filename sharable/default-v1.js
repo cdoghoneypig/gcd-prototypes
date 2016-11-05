230d4d27026285931454dcbbad8f36ef4af10873
Chuk <chukmoran@hotmail.com>
2016-11-04 17:36:44 -0700

    Javascript now assigns headers in mobile squished tables

--- sharable/default-v1.js
+++ sharable/default-v1.js
@@ -7,47 +7,49 @@
 
 // function to make the sidebar as long as wrapper1
     var elongate_Sidebar = function() {
+        // if there's no wrapper1, there's no sidebar so
+        // skip the below code and just add a top margin to the footer
         if (!document.getElementById("wrapper1")) {
             // console.log("no wrapper1");
             $("#footer").css("margin-top","4em");
             return;
         }
-
-        // console.log("Initial sidebar outerHeight at", $(".sidebar").outerHeight());
-        
-        // sum of heights of child elements in sidebar div
-        // excluding the toggle button, which is height 100%
-        var sum_of_children = 0
-        $(".sidebar").children().not(".btn-sidebartoggle").each(function(){
-            sum_of_children += $(this).outerHeight(true);
-            console.log($(this),$(this).outerHeight(true));
-        });
-        // console.log("sum of children",sum_of_children);
-
-        // height of wrapper 1
-        var height_of_wrapper1 = $("#wrapper1").outerHeight();
-        // console.log("wrapper1",height_of_wrapper1);
-
-        var min_height = 920;
-        // using 900 as a minimum
-        var best_sidebar_height = Math.max(sum_of_children, height_of_wrapper1, min_height);
-        // some fudge factor
-        var margin_of_error = 40;
-        // console.log("best sidebar height is",best_sidebar_height);
-        $(".sidebar").height(best_sidebar_height + margin_of_error);
-        
-        // if sidebar length changes, shift footer top
-        if (best_sidebar_height === min_height) {
-            $("#footer").css("top",best_sidebar_height+60);
-            // console.log("footer top set at min",best_sidebar_height+60)
-            // for issue page this is 40 over...
-
-        }
         else {
-            $("#footer").css("top",best_sidebar_height+100);
-            // console.log("footer top set at",best_sidebar_height+100)
-        }
-        
+            // console.log("Initial sidebar outerHeight at", $(".sidebar").outerHeight());
+            
+            // sum of heights of child elements in sidebar div
+            // excluding the toggle button, which is height 100%
+            var sum_of_children = 0
+            $(".sidebar").children().not(".btn-sidebartoggle").each(function(){
+                sum_of_children += $(this).outerHeight(true);
+                // console.log($(this),$(this).outerHeight(true));
+            });
+            // console.log("sum of children",sum_of_children);
+
+            // height of wrapper 1
+            var height_of_wrapper1 = $("#wrapper1").outerHeight();
+            // console.log("wrapper1",height_of_wrapper1);
+
+            var min_height = 920;
+            // using 900 as a minimum
+            var best_sidebar_height = Math.max(sum_of_children, height_of_wrapper1, min_height);
+            // some fudge factor
+            var margin_of_error = 40;
+            // console.log("best sidebar height is",best_sidebar_height);
+            $(".sidebar").height(best_sidebar_height + margin_of_error);
+            
+            // if sidebar length changes, shift footer top
+            if (best_sidebar_height === min_height) {
+                $("#footer").css("top",best_sidebar_height+60);
+                // console.log("footer top set at min",best_sidebar_height+60)
+                // for issue page this is 40 over...
+
+            }
+            else {
+                $("#footer").css("top",best_sidebar_height+100);
+                // console.log("footer top set at",best_sidebar_height+100)
+            }
+        };
 
     }
 
@@ -85,11 +87,64 @@
       };
     };
 
+
 // sidebar stuff to run on page load
     elongate_Sidebar();
     $(autocollapse_Sidebar);
     $(window).resize(autocollapse_Sidebar);
     $(window).resize(elongate_Sidebar);
+
+// set td:before for tables so they squish better
+    // make a list of all table classes for tables
+    // this could be a problem if we later add more classes to tables!
+    var table_Headers_For_Mobile = function() {
+        if (! $("table").length) {
+            // if no tables, skip the rest
+            return;
+        };
+        var table_classes = [];
+        $("table").each( function() {
+            var this_table_class_list = $(this).attr("class").split(' ')        
+            table_classes.push(this_table_class_list);
+            table_classes = [].concat.apply([], table_classes);
+        });
+
+        // remove the generic class .listing from this array
+        table_classes = table_classes.filter( function(item) 
+                {return item != "listing"});
+
+        // prepare a chunk of css style
+        var new_css = "@media only screen and (max-width: 760px), \
+        (min-device-width: 768px) and (max-device-width: 1024px)  {\n"
+
+        // for each table, extract text from each th
+        for (i = 0; i < $("table").length; i++) { 
+            var cur_table = $("table." + table_classes[i]);
+            var table_headers = []
+            cur_table.find("th").each(function(index) 
+                 {  // we replace single quotes with an escaped single quote
+                    var header_text = $.trim($(this).text()).split("'").join("\'")
+                    table_headers.push(header_text) });
+
+            for (j = 0; j < table_headers.length; j++) {
+                // use text from header in css rule for td within a table of a class
+
+                var new_rule = "." + table_classes[i] + " td:nth-of-type("
+                    + (j+1).toString() + "):before { content: \""
+                    + table_headers[j] + "\"; }\n"
+                new_css += new_rule
+                // when done maunally, the css rule looks like this
+                // .series-timeline-table td:nth-of-type(1):before { content: "Key Date"; }
+            };
+        };
+        new_css +="\n}"
+        $("<style>").prop("type", "text/css").html(new_css).appendTo("head")
+        console.log("Created new style rules for table classes:", table_classes);
+    };
+
+
+// table stuff for on page load
+    table_Headers_For_Mobile();
 
 // one-time action to set min-height of wrapper2 to match sidebar - footer height
     $("#wrapper2").css("min-height", $(".sidebar").outerHeight() );
